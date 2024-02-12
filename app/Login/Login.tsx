da8956350329af9614e407024b7ef820156db723
Yahya TAQSI <yahyataqsi@gmail.com>
2024-02-12 16:47:06 +0100

    working on responsive sigin

--- app/Login/Login.tsx
+++ app/Login/Login.tsx
@@ -1,3 +1,4 @@
+"use clinet";
 import "./Login.css";
 import log_in_out from "../../public/login.png";
 import raqeta from "../../public/rakita.png";
@@ -5,25 +6,36 @@ import apb from "../../public/apb.png";
 
 import Image from "next/image";
 import SignIn from "./SignIn";
+import { useState } from "react";
 
 export default function Login() {
+	const [signInUp, setSignInUp] = useState(false);
+
+	const switchSides = () => {
+		setSignInUp((prev) => !prev);
+	};
+
 	return (
 		<div className="login_container">
 			<div className="sign_in_out">
+				<input type="checkbox" name="check" id="check" />
 				<div className="sign_in">
-					<h1>sign in</h1>
-					<h3>use you're ApexPongBattle account to continue</h3>
-					<SignIn />
+					<div className="sign_in_header">
+						<h1>sign in</h1>
+						<h3>use you're ApexPongBattle account to continue</h3>
+					</div>
+					<SignIn signInUp={signInUp} />
 					<h5>
-						you don't have an account? <label htmlFor="check">Sing Up.</label>
+						you don't have an account?{" "}
+						<label onClick={switchSides} htmlFor="check">
+							Sing {signInUp ? "In" : "Up"}.
+						</label>
 					</h5>
-					<div className="logo">
+					<div className="login_logo">
 						<Image className="logo_img" src={raqeta} alt="logo_img" />
 						<Image className="logo_img" src={apb} alt="logo_name" />
 					</div>
 				</div>
-				<div className="sign_up"></div>
-				<input type="checkbox" name="check" id="check" />
 				<Image className="log_in_out_img" src={log_in_out} alt="log_in_out" />
 			</div>
 		</div>
--- app/Login/SignIn.tsx
+++ app/Login/SignIn.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./SignIn.css";
 import { FaArrowRight } from "react-icons/fa6";
 
@@ -7,23 +8,39 @@ import Image from "next/image";
 import ggl from "../../public/ggl_icon.png";
 import intra from "../../public/42_logo.svg";
 
-export default function SignIn() {
+export default function SignIn({ signInUp }) {
 	return (
 		<div className="sign_in_container">
-			<input placeholder="username" className="sign_in_ships" type="text" />
-			<input placeholder="password" className="sign_in_ships" type="text" />
-			<button className="sign_in_ships btn">
-				Let's play <FaArrowRight />
-			</button>
+			<div className="sign_in_input_div">
+				<input placeholder="username" className="sign_in_ships" type="text" />
+			</div>
+			{signInUp && (
+				<div className="sign_in_input_div">
+					<input placeholder="email" className="sign_in_ships" type="text" />
+				</div>
+			)}
+			<div className="sign_in_input_div">
+				<input
+					placeholder="password"
+					className="sign_in_ships"
+					type="password"
+				/>
+			</div>
+			<div className="sign_in_input_div">
+				<button className="sign_in_ships btn">
+					Let's play <FaArrowRight />
+				</button>
+			</div>
+
 			<h1>OR</h1>
 			<div className="outher_methods">
 				<button className="other_login">
 					{" "}
-					<Image src={ggl} width={20} height={20} alt="google" /> google
+					<Image src={ggl} width={26} height={26} alt="google" /> google
 				</button>
 				<button className="other_login">
 					{" "}
-					<Image src={intra} width={20} height={20} alt="google" /> intra
+					<Image src={intra} width={26} height={26} alt="google" /> intra
 				</button>
 			</div>
 		</div>
