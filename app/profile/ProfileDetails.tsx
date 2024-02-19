63a0cde25dd5880d038458c99fd6b49eb96b1a82
Yahya TAQSI <yahyataqsi@gmail.com>
2024-02-19 15:05:32 +0100

    merging with yassin

--- app/profile/ProfileDetails.tsx
+++ app/profile/ProfileDetails.tsx
@@ -1,31 +1,40 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import "./ProfileDetails.css";
 import Friends from "./Friends";
 
 export default function ProfileDetails() {
+	const [switchElements, setSwitchElements] = useState("friends");
 
-    const [switchElements, setSwitchElements] = useState('friends')
-
-
-    const switchE = (e) =>{
-        setSwitchElements(e.target.textContent)
-    }
-	return <div className="profile_details_container">
-        <div className="profile_details_header">
-            <span 
-            onClick={switchE}
-            className={`${switchElements === 'stats' && 'switch_e'}`}>stats</span>
-            <span 
-            onClick={switchE}
-            className={`${switchElements === 'friends' && 'switch_e'}`}>friends</span>
-            <span 
-            onClick={switchE}
-            className={`${switchElements === 'achievements' && 'switch_e'}`}>achievements</span> 
-        </div>
-        <div className="profile_details_data">
-            {switchElements === 'friends' &&  <Friends/>}
-        </div>
-    </div>;
+	const switchE = (e) => {
+		setSwitchElements(e.target.textContent);
+	};
+	return (
+		<div className="profile_details_container">
+			<div className="profile_details_header">
+				<span
+					onClick={switchE}
+					className={`${switchElements === "stats" && "switch_e"}`}
+				>
+					stats
+				</span>
+				<span
+					onClick={switchE}
+					className={`${switchElements === "friends" && "switch_e"}`}
+				>
+					friends
+				</span>
+				<span
+					onClick={switchE}
+					className={`${switchElements === "achievements" && "switch_e"}`}
+				>
+					achievements
+				</span>
+			</div>
+			<div className="profile_details_data">
+				{switchElements === "friends" && <Friends />}
+			</div>
+		</div>
+	);
 }
