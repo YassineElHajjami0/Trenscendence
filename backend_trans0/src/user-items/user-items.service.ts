9786f6416f694e43200e9a62bd26df79ff847c2a
Ayoub Mentag <amentag@e2r3p15.1337.ma>
2024-04-21 13:40:55 +0100

    userItems loading ...

--- backend_trans0/src/user-items/user-items.service.ts
+++ backend_trans0/src/user-items/user-items.service.ts
@@ -7,24 +7,45 @@ import { DatabaseService } from 'src/database/database.service';
 export class UserItemsService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  create(createUserItemDto: Prisma.UserItemCreateInput) {
+  async create(createUserItemDto: Prisma.UserItemCreateInput) {
     // if (createUserItemDto) return 'This action adds a new userItem';
     return this.databaseService.userItem.create({ data: createUserItemDto });
   }
 
-  findAll() {
+  async findAll() {
     return this.databaseService.userItem.findMany({});
   }
 
-  findOne(id: number) {
+  async findUserItems(userId: number) {
+    console.log(userId);
+    const userItems = await this.databaseService.userItem.findMany({
+      select: {
+        itemId: true,
+      }
+    });
+    const userItemsId = userItems.map((item) => item.itemId);
+
+    const allItems = await this.databaseService.item.findMany();
+    const allItemsModified = allItems.map((item) => {
+      const newItem = { ...item, choosed: false, unlocked: false };
+      if (item.id in userItemsId) {
+        newItem.unlocked = true;
+        newItem.choosed = false; // not always false
+      }
+      return newItem;
+    });
+    return allItemsModified;
+  }
+
+  async findOne(id: number) {
     return this.databaseService.userItem.findMany({ where: { userId: id } });
   }
 
-  update(id: number, updateUserItemDto: Prisma.UserItemUpdateInput) {
+  async update(id: number, updateUserItemDto: Prisma.UserItemUpdateInput) {
     if (updateUserItemDto) return `This action updates a #${id} userItem`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return this.databaseService.userItem.delete({
       where: { userId_itemId: { userId: id, itemId: id } },
     });
