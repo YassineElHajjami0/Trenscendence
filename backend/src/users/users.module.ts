f936221d3c510dd437a10ebe9f8e36d6019f1fd5
Yahya TAQSI <yahyataqsi@gmail.com>
2024-07-16 16:53:06 +0100

    fix old commit dyal mimoun o ayoub

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -7,7 +7,7 @@ import { DatabaseService } from 'src/database/database.service';
 
 import { UserAchievementModule } from 'src/user-achievement/user-achievement.module';
 import { ChatGateway } from 'src/chatSockets/chat.getway';
-import { GameGateway } from 'src/game/game.gateway';
+
 
 @Module({
   imports: [
@@ -16,7 +16,7 @@ import { GameGateway } from 'src/game/game.gateway';
     UserAchievementModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService, DatabaseService, ChatGateway, GameGateway],
+  providers: [UsersService, DatabaseService, ChatGateway],
   exports: [UsersService],
 })
 export class UsersModule {}
